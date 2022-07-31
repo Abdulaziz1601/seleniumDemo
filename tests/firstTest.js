6a7242ece7879434ede04fed8be3315980516edc
Abdulaziz <abdulazizfaactory@gmail.com>
2022-07-31 18:14:32 +0500

    working with selenium webdriver

---
+++ tests/firstTest.js
@@ -0,0 +1,21 @@
+const { Builder, Key, By } = require("selenium-webdriver");
+const assert = require("assert"); // built in nodejs assertion 
+
+
+
+async function example() {
+    // launch the browser
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    // Navigate to our application
+    await driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+    // Add a todo
+    await driver.findElement(By.id('sampletodotext')).sendKeys("Learn Selenium", Key.RETURN);
+    // close the browser
+    await driver.quit();
+
+}
+
+
+example();
